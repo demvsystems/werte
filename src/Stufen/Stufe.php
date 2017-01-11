d1693b2baca8991b98ded6436d73221bb17dc367
Tareq Kellyeh <tareq.kellyeh@gmail.com>
2017-01-11 16:00:05 +0100

    Stufen hizufügen #8

---
+++ src/Stufen/Stufe.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Tareq
+ * Date: 1/11/2017
+ * Time: 3:36 PM
+ */
+interface Stufe
+{
+    const FINANZIELLE_GRUNDBEDUERFNISSE    = 1;
+    const ERHALTUNG_DES_LEBENSSTANDARDS    = 2;
+    const VERBESSERUNG_DES_LEBENSSTANDARDS = 3;
+}
