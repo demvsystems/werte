0a46b9d4294bb91fa69bd36c77508ecbdc482f44
Alex <a.mihm@demv.de>
2017-01-16 09:47:14 +0100

    Rechtsschutz untersparten hinzugefügt

--- src/Sparte/Sparten/RechtsschutzPrivat.php
+++ src/Sparte/Sparten/RechtsschutzPrivat.php
@@ -14,4 +14,10 @@ namespace Demv\Werte\Sparte\Sparten;
 final class RechtsschutzPrivat
 {
     const ID = 169;
+
+    const VERMIETER          = 367;
+    const MIETER_EIGENTUEMER = 366;
+    const BERUF              = 365;
+    const VERKEHR            = 364;
+    const PRIVAT             = 363;
 }
