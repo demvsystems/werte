784f2406015fd8abe98dc5a7e079de6f8504bb41
Keno Weßels <k.wessels@demv.de>
2017-01-24 09:42:21 +0100

    Added class NoneTaetigkeitsstatus

---
+++ src/Person/Taetigkeitsstatus/Status/NoneTaetigkeisstatus.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: keno
+ * Date: 24.01.17
+ * Time: 09:32
+ */
+
+namespace Demv\Werte\Person\Taetigkeitsstatus\Status;
+
+use Demv\Werte\NoneValue;
+use Demv\Werte\Person\Taetigkeitsstatus\TaetigkeitsstatusInterface;
+
+final class NoneTaetigkeisstatus extends NoneValue implements TaetigkeitsstatusInterface
+{
+    /**
+     * @return bool
+     */
+    public function isErwerbstaetig(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isArbeitnehmer(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasEntgeltfortzahlung(): bool
+    {
+        return false;
+    }
+}
