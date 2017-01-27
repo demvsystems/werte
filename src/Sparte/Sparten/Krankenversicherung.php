1f62f22fe73d1fe495f4e52fd5b3fafd75930f56
Christian <chaberland@gmx.net>
2017-01-27 15:33:43 +0100

    Krankenversicherung::KRANKENTAGEGELD (159) in Werte Package

---
+++ src/Sparte/Sparten/Krankenversicherung.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Demv\Werte\Sparte\Sparten;
+
+/**
+ * Class Krankenversicherung
+ * @package Demv\Werte\Sparte\Sparten
+ */
+final class Krankenversicherung
+{
+    const KRANKENTAGEGELD = 159;
+}
