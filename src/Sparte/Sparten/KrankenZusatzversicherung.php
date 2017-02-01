28538a1f807513e2837312c6f77721dbf8d1ad9c
Christian <c.haberland@demv.de>
2017-02-01 10:33:21 +0100

    Gesundheitssparten einpflegen #23

---
+++ src/Sparte/Sparten/KrankenZusatzversicherung.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Demv\Werte\Sparte\Sparten;
+
+/**
+ * Interface Krankenzusatzversicherung
+ * @package Demv\Werte\Sparte\Sparten
+ */
+interface KrankenZusatzversicherung
+{
+    /**
+     * 160    Krankenversicherung->Krankenzusatzversicherung->Pflegetagegeld
+     */
+    const PFLEGETAGEGELD = 160;
+
+    /**
+     * 291    Krankenversicherung->Krankenzusatzversicherung->Pflegekostenversicherung
+     */
+    const PFLEGEKOSTENVERSICHERUNG = 291;
+}
--- src/Sparte/Sparten/Krankenversicherung.php
+++ src/Sparte/Sparten/Krankenversicherung.php
@@ -9,4 +9,19 @@ namespace Demv\Werte\Sparte\Sparten;
 interface Krankenversicherung
 {
     const KRANKENTAGEGELD = 159;
+
+    /**
+     * 158    Krankenversicherung->Gesetzliche Krankenversicherung
+     */
+    const GESETZLICHE_KRANKENVERSICHERUNG = 158;
+
+    /**
+     * 156    Krankenversicherung->Private Krankenvollversicherung
+     */
+    CONST PRIVATE_KRANKENVOLLVERSICHERUNG = 156;
+
+    /**
+     * 368    Krankenversicherung->Freie Heilfürsorge
+     */
+    const FREIE_HEILFUERSORGE = 368;
 }
