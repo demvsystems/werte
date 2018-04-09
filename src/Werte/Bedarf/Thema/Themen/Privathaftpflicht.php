ccdc460eaefa51ef8b55929c6ac58e25323930fd
Keno Weßels <kenowessels@googlemail.com>
2018-04-09 11:29:27 +0200

    fix

--- src/Werte/Bedarf/Thema/Themen/Privathaftpflicht.php
+++ src/Werte/Bedarf/Thema/Themen/Privathaftpflicht.php
@@ -17,6 +17,6 @@ final class Privathaftpflicht extends Bedarfsthema
      */
     public function __construct()
     {
-        parent::__construct(self::ID, 'Privathaftpflicht', [self::ID]);
+        parent::__construct(self::ID, 'Privathaftpflicht', [\Demv\Werte\Sparte\Sparten\Privathaftpflicht::ID]);
     }
 }
