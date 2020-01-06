076371285a7aa38ecc9ab5e322f673d14720ae39
Alex <a.mihm@demv.de>
2020-01-06 12:28:48 +0100

    Bemessungsgrenzen für 2020 angepasst

--- src/Werte/Beitragsbemessung/BBG.php
+++ src/Werte/Beitragsbemessung/BBG.php
@@ -11,16 +11,16 @@ final class BBG
     /**
      * Versicherungspflichtgrenze
      */
-    const JAEG = 60750;
+    const JAEG = 62550;
 
     /**
      * Bemessungsgrenze für Kranken- und Pflegeversicherungen
      */
-    const KRANKEN_UND_PFLEGE = 54450;
+    const KRANKEN_UND_PFLEGE = 56250;
 
     /**
      * Bemessungsgrenze für die gesetzliche Rentenversicherung
      * Hier wird der Durchschnitt von West und Ost genommen
      */
-    const GESETZLICHE_RENTE = (80400 + 73800) / 2;
+    const GESETZLICHE_RENTE = (82800 + 77400) / 2;
 }
