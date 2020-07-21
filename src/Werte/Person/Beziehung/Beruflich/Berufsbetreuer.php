76de58f4cd7a62d5e533e3ca199df34876d5bcaa
Alexander Achim <a.achim@demv.de>
2020-07-21 17:16:25 +0200

    add new relationship Berufsbetreuer & Betreuter

---
+++ src/Werte/Person/Beziehung/Beruflich/Berufsbetreuer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Demv\Werte\Person\Beziehung\Beruflich;
+
+/**
+ * Class Berufsbetreuer
+ * @package Demv\Werte\Person\Beziehung\Beruflich
+ */
+final class Berufsbetreuer extends BeruflicheBeziehung
+{
+    public const ID = 33;
+
+    public function __construct()
+    {
+        parent::__construct(self::ID, 'Berufsbetreuer');
+    }
+}
