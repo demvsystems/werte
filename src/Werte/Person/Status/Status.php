b19d676d7860be15ba3ea34d4a8ce269d6cc2831
Paweł Jadanowski <pjadanowski@gmail.com>
2026-02-03 16:32:57 +0100

    Add User Status value objects
    Implement user status types (Aktiv, Passiv, Storniert, Interessiert, NeuerPartner, Unbekannt) using the existing provider pattern to replace hardcoded status mappings.

---
+++ src/Werte/Person/Status/Status.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Demv\Werte\Person\Status;
+
+enum Status: int
+{
+    case AKTIV         = 1;
+    case PASSIV        = 2;
+    case STORNIERT     = 3;
+    case INTERESSIERT  = 4;
+    case NEUER_PARTNER = 5;
+    case UNBEKANNT     = 0;
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::AKTIV         => 'Aktiv',
+            self::PASSIV        => 'Passiv',
+            self::STORNIERT     => 'Storniert',
+            self::INTERESSIERT  => 'Interessiert',
+            self::NEUER_PARTNER => 'Neuer Partner',
+            self::UNBEKANNT     => 'Unbekannt',
+        };
+    }
+}
